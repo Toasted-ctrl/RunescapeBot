c16a185b0382bb863d63d8264fc4b35a6ed176f7
MB-dev-875 <themickbb@gmail.com>
2024-08-26 20:45:54 +0200

    Implemented checkDatabasePresence. Checks if requested username is present in all tables. Should replace the check through status return in future update.

--- commandInputChecker.py
+++ commandInputChecker.py
@@ -1,4 +1,21 @@
 import re
+import os
+import datetime
+import pandas as pd
+from sqlalchemy import create_engine
+from dotenv import load_dotenv, dotenv_values
+
+load_dotenv()
+
+db_database = os.getenv("db_database")
+db_password = os.getenv("db_password")
+db_user = os.getenv("db_user")
+db_hostname = os.getenv("db_hostname")
+db_port_id = os.getenv("db_port_id")
+db_method_db = os.getenv("db_method_db")
+db_method_conn = os.getenv("db_method_conn")
+
+engine = create_engine(f"{db_method_db}+{db_method_conn}://{db_user}:{db_password}@{db_hostname}:{db_port_id}/{db_database}")
 
 def checkInputDate(insertedDate):
     
@@ -37,3 +54,41 @@ def checkInputString(insertedString):
 
     elif result == False:
         return(1)
+    
+def checkDatabasePresence(insertedPlayerName):
+
+    #create currentDate datestamp, to allow filtering dataframes with today's date
+    currentDateNum = datetime.date.today()
+    currentDate = str(currentDateNum)
+
+    string_hiscores = str(f"SELECT * FROM main_runescape_hiscores WHERE player_name = '{insertedPlayerName}' AND datesync_date ='{currentDate}'")
+    string_status = str(f"SELECT * FROM main_runescape_status WHERE player_name = '{insertedPlayerName}' AND datesync_date ='{currentDate}'")
+    string_achievements = str(f"SELECT * FROM main_runescape_achievements WHERE player_name = '{insertedPlayerName}' AND datesync_date ='{currentDate}'")
+    string_activities = str(f"SELECT * FROM main_runescape_activities_processed WHERE player_name = '{insertedPlayerName}'")
+
+    df_hiscores = pd.read_sql(sql=string_hiscores, con=engine)
+    df_status = pd.read_sql(sql=string_status, con=engine)
+    df_achievements = pd.read_sql(sql=string_achievements, con=engine)
+    df_activities = pd.read_sql(sql=string_activities, con=engine)
+
+    if df_hiscores.empty:
+        hiscores_present = 0
+    else: 
+        hiscores_present = 1
+
+    if df_status.empty:
+        status_present = 0
+    else: 
+        status_present = 1
+
+    if df_achievements.empty:
+        achievements_present = 0
+    else: 
+        achievements_present = 1
+
+    if df_activities.empty:
+        activities_present = 0
+    else: 
+        activities_present = 1
+
+    return (hiscores_present, status_present, achievements_present, activities_present)
