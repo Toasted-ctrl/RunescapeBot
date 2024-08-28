eccc3f27afc3dba1751518b69e620ddc509cf1eb
MB-dev-875 <themickbb@gmail.com>
2024-08-28 19:23:03 +0200

    Bugfix in checkInputString.

--- commandInputChecker.py
+++ commandInputChecker.py
@@ -50,7 +50,7 @@ def checkInputString(insertedString):
     result = any(i in insertedString for i in char_list)
 
     if result == True:
-        return(0)
+        return([0])
 
     elif result == False:
 
